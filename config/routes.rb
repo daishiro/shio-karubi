d833415040f75249b17222b11deea28e47467f8f
daishiro <high.response@gmail.com>
2012-09-06 16:46:59 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ShioKarubi::Application.routes.draw do
+  get "pages/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,3 +58,5 @@ ShioKarubi::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
+root :to => 'pages#index'
