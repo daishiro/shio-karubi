9ea14b2f15b6b0b42e96efeb31ce0c4b60cac2d8
daishiro <high.response@gmail.com>
2012-09-06 15:59:15 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShioKarubi::Application.config.secret_token = '809aa70a388b1c8933eb7d74db9aa58e6b3b0bdf01333b3446618f4da5069e9c3428bde14ab22881b8c4ebfb34b0afd7a27e953ce9442ecbdb76e41624485386'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShioKarubi::Application.config.session_store :cookie_store, key: '_shio-karubi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShioKarubi::Application.config.session_store :active_record_store
